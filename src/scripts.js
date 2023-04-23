dd54c2f333b007a8896eba7aa39ab1738b527e9a
Devynne Marshall <devynnem@gmail.com>
2023-04-23 15:52:29 -0600

    feat:Able to see upcoming pending trips printed to the DOM

--- src/scripts.js
+++ src/scripts.js
@@ -26,7 +26,10 @@ let pendingTrips = document.querySelector("#pendingTripsButton")
 let totalSpentOnTripsThisYear = document.querySelector("#totalSpentOnTripsThisYear")
 let calendar =document.querySelector("#calendar")
 let form = document.querySelector("#travelerRequests");
-let tavelerInput = document.querySelector("#tavelerInput");
+let destinationInput = document.querySelector("#destinationInput");
+let numberOfTravelers = document.querySelector("#numberOfTravelers");
+let duration = document.querySelector("#numberOfDays");
+let newEntry = document.querySelector('#newEntry');
 
 // let tripsContainer = document.querySelector("#tripsContainer")
 
@@ -34,6 +37,9 @@ let tavelerInput = document.querySelector("#tavelerInput");
 let currentTraveler, allTripsPrinted, pastTripsPrinted, pendingTripsPrinted, traveler, trip
 let date = new Date();
 let currentDate = date.getFullYear() + "/" + ("0" + (date.getMonth()+1)).slice(-2) + "/"+ ("0" + date.getDate()).slice(-2);
+// let destinationID = trip
+
+// console.log(destinationID)
 // let traveler = new Traveler (travelers);
 // let trip = new Trip (trips, destinations)
 
@@ -47,28 +53,13 @@ window.addEventListener('load', function() {
   })
 })
 
+
 allTrips.addEventListener('click', seeAllTrips)
 pastTrips.addEventListener('click', seePastTrips)
 pendingTrips.addEventListener('click', seePendingTrips)
 // dropdown.addEventListener('click', createDropdown)
 
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   const data = {
-//     "userID": newUser.id, 
-//     "date": document.getElementById('dateInput').value.split('-').join('/'), 
-//     "travelers": tavelerInput.value,
-   //  "duration": 
-  //   "status": "pending" 
-//     {"id": , 
-//       "userID": currentTraveler.id, 
-//         "destinationID": <number>, 
-//           "travelers": <number>, 
-//             "date": document.getElementById('dateInput').value.split('-').join('/'), 
-//             "duration": <number>, 
-//               "status": <string 'approved' or 'pending'>, 
-//               "suggestedActivities": <array of strings>}
-//   };
+
 
 //   fetch('http://localhost:3001/api/v1/hydration', {
 //     method: 'POST',
@@ -93,7 +84,7 @@ function displayTravelerInfo() {
 }
 
 function displayCalendar() {
-  calendar.innerHTML = `<input id="dateInput" type="date" max="${currentDate.split('/').join('-')}" name="date" placeholder="yyyy/mm/dd" required>`;
+  calendar.innerHTML = `<input id="dateInput" type="date" min="${currentDate.split('/').join('-')}" name="date" placeholder="yyyy/mm/dd" required>`;
   // calendar2.innerHTML = `<input id="dateInput2" type="date" max="${currentDate.split('/').join('-')}" name="date" placeholder="yyyy/mm/dd" required>`;
 };
 function generateRandomUser() {
@@ -161,26 +152,91 @@ function displayPastTrips(tripsData) {
     </section>`
   });
 };
-
-function displayPendingTrips(tripsData) {
-  pendingTripsView.innerHTML = "";
-  tripsData.forEach(trips => {
-    const destination = trip.findDestinationById(trips.destinationID);
-    pendingTripsView.innerHTML +=
-    `<section class="trip-card-template">
-    <img class="card-image" alt="${destination.alt}" src="${destination.image}" />
-    <section class="trip-details-container">
-      <p class="trip trip-name">Going To: ${destination.destination}</p>
-      <p class="trip date">Left On: ${trips.date}</p>
-      <p class="trip number-of-travelers">${trips.travelers} Travelers</p>
-      <p class="trip duration">${trips.duration} Days</p>
-      <p class="trip status">Status: ${trips.status}</p>
-    </section>
-    </section>`
-  });
-}
+{/* <p class="trip date">Leave On: ${trips.date}</p>
+<p class="trip number-of-travelers">${trips.travelers} Travelers</p>
+<p class="trip duration">${trips.duration} Days</p>
+<p class="trip status">Status: ${trips.status}</p> */}
+// alt: "ornate buildings with a garden during the day"
+// destination:"Bangkok, Thailand"
+// estimatedFlightCostPerPerson: 988
+// estimatedLodgingCostPerDay: 35
+// id: 16
+// image: "https://images.unsplash.com/photo-1563492065599-3520f775eeed?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+// newTrip: 
+// date: "2023/04/25"
+// destinationID: 45
+// duration: "1"
+// id: 212
+// status: "pending"
+// suggestedActivities: ]
+// travelers: "1"
+// function displayPendingTrips(data) {
+//   pendingTripsView.innerHTML = "";
+//   // data.forEach(trips => {
+//   //   const destination = trip.findDestinationById(trips.destinationID);
+//     pendingTripsView.innerHTML +=
+//     `<section class="trip-card-template">
+//     <img class="card-image" alt="${data.alt}" src="${data.image}" />
+//     <section class="trip-details-container">
+//       <p class="trip trip-name">Going To: ${data.destination}</p>
+//     </section>
+//     </section>`
+//   // });
+// }
 
 function showTotalSpentThisYear() {
   totalSpentOnTripsThisYear.innerText = `Total Spent on Trips This Year: $${trip.calculateTotalCostPerYear(currentTraveler.id)}`
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // console.log("LOOK HERE", trip.destinationData[0].id)
+  const destinationId = trip.destinationData.find(destination => destination.destination === destinationInput.value);
+  console.log(destinationId)
+  const data = {
+    "id": trip.tripData.length + 1, 
+    "userID": currentTraveler, 
+    "destinationID": destinationId.id,
+    "date": document.getElementById('dateInput').value.split('-').join('/'), 
+    "travelers": numberOfTravelers.value,
+    "duration": duration.value,
+    "status": "pending",
+    "suggestedActivities": [], 
+    }
+    fetch('http://localhost:3001/api/v1/trips', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(data => data.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(`Error at: ${err}`));
+
+  displayNewTripEntry(destinationId);
+  displayPendingTrips(data, destinationId)
+  event.target.reset();
+  });
+
+  function displayNewTripEntry(destinationId) {
+  newEntry.innerText = `Your trip request for ${destinationId.destination} is pending!`;
+}
+function displayPendingTrips(data, destinationId) {
+  pendingTripsView.innerHTML = "";
+  // data.forEach(trips => {
+  //   const destination = trip.findDestinationById(trips.destinationID);
+  pendingTripsView.innerHTML +=
+    `<section class="trip-card-template">
+    <img class="card-image" alt="${destinationId.alt}" src="${destinationId.image}" />
+    <section class="trip-details-container">
+      <p class="trip trip-name">Going To: ${destinationId.destination}</p>
+      <p class="trip date">Leaving On: ${data.date}</p>
+      <p class="trip number-of-travelers">${data.travelers} Travelers</p>
+      <p class="trip duration">${data.duration} Days</p>
+      <p class="trip status">Status: ${data.status}</p>
+    </section>
+    </section>`;
+  // });
 }
 
